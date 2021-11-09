cbde68c0a1d040bae1dd44d5857bc3c27bc09b41
nascott <nascott94@outlook.com>
2021-11-09 09:54:02 -0700

    quert selector

--- DOM/app.js
+++ DOM/app.js
@@ -9,13 +9,33 @@
 // btn.style.color = "white";
 
 //TAG NAME
-const headings = document.getElementsByTagName("h2");
-headings[0].style.color = "pink";
-// console.log(headings.length);
+// const headings = document.getElementsByTagName("h2");
+// headings[0].style.color = "pink";
+// // console.log(headings.length);
 
+// // const items = document.getElementsByTagName("li");
 // const items = document.getElementsByTagName("li");
-const items = document.getElementsByTagName("li");
-const betterItems = [...items];
-betterItems.forEach(function (item) {
+// const betterItems = [...items];
+// betterItems.forEach(function (item) {
+//   console.log(item);
+// });
+
+//CLASSNAME
+// const listItems = document.getElementsByClassName("special");
+// console.log(listItems);
+
+//QUERY SELECTOR
+const result = document.querySelector("#result");
+result.style.backgroundColor = "pink";
+
+const item = document.querySelector(".special");
+console.log(item);
+
+const lastItem = document.querySelector("li:last-child");
+console.log(lastItem);
+
+const list = document.querySelectorAll(".special");
+list.forEach(function (item) {
   console.log(item);
+  item.style.color = "green";
 });
