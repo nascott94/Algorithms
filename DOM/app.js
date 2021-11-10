7187bf4e5f8f9f90f3a0639d4bfc9a36477ee292
nascott <nascott94@outlook.com>
2021-11-10 09:25:27 -0700

    parent, child, sibling practice

--- DOM/app.js
+++ DOM/app.js
@@ -25,17 +25,39 @@
 // console.log(listItems);
 
 //QUERY SELECTOR
-const result = document.querySelector("#result");
-result.style.backgroundColor = "pink";
+// const result = document.querySelector("#result");
+// result.style.backgroundColor = "pink";
 
-const item = document.querySelector(".special");
-console.log(item);
+// const item = document.querySelector(".special");
+// console.log(item);
 
-const lastItem = document.querySelector("li:last-child");
-console.log(lastItem);
+// const lastItem = document.querySelector("li:last-child");
+// console.log(lastItem);
 
-const list = document.querySelectorAll(".special");
-list.forEach(function (item) {
-  console.log(item);
-  item.style.color = "green";
-});
+// const list = document.querySelectorAll(".special");
+// list.forEach(function (item) {
+//   console.log(item);
+//   item.style.color = "green";
+// });
+
+//CHILD/PARENT
+// const result = document.querySelector("#result");
+// const allChildren = result.childNodes;
+// // console.log(allChildren);
+// const children = result.children;
+// console.log(children);
+// console.log(result.firstChild);
+// console.log(result.lastChild);
+
+//PARENT ELEMENT
+// const heading = document.querySelector("h2");
+// console.log(heading.parentElement.parentElement);
+
+//PREVIOUS SIBLING
+// const first = document.querySelector(".first");
+// const second = first.nextSibling;
+// console.log(second);
+
+// const last = document.querySelector("#last");
+// const third = last.previousSibling.previousSibling;
+// console.log(third);
