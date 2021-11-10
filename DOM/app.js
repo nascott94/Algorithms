595d8079049393d07761f0cc6f8dc2b8ac02cc03
nascott <nascott94@outlook.com>
2021-11-10 09:57:12 -0700

    classname and class list

--- DOM/app.js
+++ DOM/app.js
@@ -61,3 +61,36 @@
 // const last = document.querySelector("#last");
 // const third = last.previousSibling.previousSibling;
 // console.log(third);
+
+//NODE VALUE/TEXT CONTENT
+// const item = document.getElementById("special");
+// const value = item.firstChild.nodeValue;
+// console.log(item.childNodes);
+
+//getAttributes,setAttributes
+// const first = document.querySelector(".first");
+// const idValue = first.getAttribute("id");
+// console.log(idValue);
+
+// const link = document.getElementById("link");
+// const showLink = link.getAttribute("href");
+// console.log(showLink);
+
+// const last = link.nextElementSibling;
+// last.setAttribute("class", "first");
+// last.textContent = "i also have a class of first";
+// console.log(last);
+
+// const links = document.querySelectorAll(".first");
+// console.log(links);
+
+//CLASSNAME/CLASSLIST
+const first = document.getElementById("first");
+const second = document.getElementById("second");
+const third = document.getElementById("third");
+
+const classValue = first.className;
+console.log(classValue);
+
+second.className = "colors text";
+third.classList.add("colors");
