4df6c7e6a843a325efe3e1a6dc6ec635e881ba0d
nascott <nascott94@outlook.com>
2021-11-18 09:40:03 -0700

    addeventlistner practice

--- DOM/app.js
+++ DOM/app.js
@@ -101,3 +101,23 @@
 // const bodyDiv = document.createElement("div");
 // //create text node
 // const text = document.createTextNode;
+
+// // select element
+// // addEventListener()
+// // what event, what to do
+
+// const btn = document.querySelector(".btn");
+// const heading = document.querySelector("h2");
+
+// function changeColors() {
+//   let hasClass = heading.classList.contains("red");
+//   if (hasClass) {
+//     heading.classList.remove("red");
+//   } else {
+//     heading.classList.add("red");
+//   }
+// }
+// // btn.addEventListener("click", function () {
+// //   heading.classList.add("red");
+// // });
+// btn.addEventListener("click", changeColors);
