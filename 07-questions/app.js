fde1f620637225a696cb409f63357c4b0306a0fb
nascott <nascott94@outlook.com>
2021-12-08 15:28:39 -0700

    using selectors inside the element

--- 07-questions/app.js
+++ 07-questions/app.js
@@ -1,11 +1,15 @@
 //using selectors inside the element
-// traversing the dom
-
-const btns = document.querySelectorAll(".question-btn");
-
-btns.forEach(function (btn) {
-  btn.addEventListener("click", function (e) {
-    const question = e.currentTarget.parentElement.parentElement;
-    question.classList.toggle("show-text");
-  });
+const questions = document.querySelectorAll(".question");
+questions.forEach(function (orange) {
+  console.log(orange);
 });
+
+// traversing the dom
+// const btns = document.querySelectorAll(".question-btn");
+
+// btns.forEach(function (btn) {
+//   btn.addEventListener("click", function (e) {
+//     const question = e.currentTarget.parentElement.parentElement;
+//     question.classList.toggle("show-text");
+//   });
+// });
